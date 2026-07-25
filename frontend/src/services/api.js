8dc5c493c72c3986dd2b109a1d7830a4730d3d1d
Harsh Kaushal <harshkaushal302@gmail.com>
2026-07-25 21:55:20 +0530

    some changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getStoredToken } from '../utils/authStorage';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: "/api/v1",
   headers: {
     'Content-Type': 'application/json',
   },
